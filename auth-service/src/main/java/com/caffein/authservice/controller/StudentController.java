07f29fd7ebc445ad7df7c876ca369fdb4aa51e06
sinh nguyen <nsinh2281@gmail.com>
2025-10-26 00:25:15 +1100

    route for admin to create user and debug permission

--- auth-service/src/main/java/com/caffein/authservice/controller/StudentController.java
+++ auth-service/src/main/java/com/caffein/authservice/controller/StudentController.java
@@ -2,10 +2,7 @@ package com.caffein.authservice.controller;
 
 import com.caffein.authservice.kafka.producer.UserProducer;
 import com.caffein.authservice.model.User;
-import com.caffein.authservice.request.authRequest.AuthenticationRequest;
-import com.caffein.authservice.request.authRequest.RefreshRequest;
 import com.caffein.authservice.request.authRequest.RegistrationRequest;
-import com.caffein.authservice.response.AuthenticationResponse;
 import com.caffein.authservice.service.auth.AuthenticationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -19,34 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/student")
 @RequiredArgsConstructor
-@Tag(name = "Authentication", description = "Authentication API")
-public class AuthenticationController {
+@Tag(name = "student", description = "Create a student API")
+public class StudentController {
 
     private final AuthenticationService service;
     private final UserProducer userProducer;
 
-    @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> login(
-            @Valid
-            @RequestBody
-            final AuthenticationRequest request) {
-        return ResponseEntity.ok(this.service.login(request));
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<Void> register(@Valid @RequestBody final RegistrationRequest request) {
-        User user = this.service.register(request);
+    @PostMapping("/admin/register-student")
+    @PreAuthorize("hasAuthority('admin:write')")
+    public ResponseEntity<Void> registerStudent(@Valid @RequestBody final RegistrationRequest request) {
+        User user = this.service.registerANewStudent(request);
         userProducer.authToStudentTopic("pushUserFromAuthServiceToStudentServiceTopic", user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-
-    @PostMapping("/refresh")
-    public ResponseEntity<AuthenticationResponse> refresh(
-            @RequestBody
-            final RefreshRequest req) {
-        return ResponseEntity.ok(this.service.refreshToken(req));
     }
 }
--- auth-service/src/main/java/com/caffein/authservice/controller/UserController.java
+++ auth-service/src/main/java/com/caffein/authservice/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.caffein.authservice.controller;
 
-
 import com.caffein.authservice.model.User;
 import com.caffein.authservice.request.roleRequest.UserRoleUpdateRequest;
 import com.caffein.authservice.request.userRequest.ChangePasswordRequest;
@@ -12,7 +11,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -28,34 +26,35 @@ public class UserController {
 
     @PatchMapping("/me")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void updateProfileInfo(@Valid final ProfileUpdateRequest request, final Authentication principal){
+    public void updateProfileInfo(@Valid final ProfileUpdateRequest request, final Authentication principal) {
         this.userService.updateProfileInfo(request, getUserId(principal));
 
     }
 
     @PostMapping("/me/password")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void changePassword(@RequestBody @Valid final ChangePasswordRequest request, final Authentication principal){
+    public void changePassword(@RequestBody @Valid final ChangePasswordRequest request,
+            final Authentication principal) {
         this.userService.changePassword(request, getUserId(principal));
 
     }
 
     @PatchMapping("/deactivate")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void deactivateAccount(final Authentication principal){
+    public void deactivateAccount(final Authentication principal) {
         this.userService.deactivateAccount(getUserId(principal));
     }
 
     @PatchMapping("/reactivate")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void reactivateAccount(final Authentication principal){
+    public void reactivateAccount(final Authentication principal) {
         this.userService.reactivateAccount(getUserId(principal));
     }
 
     @DeleteMapping("/me")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @PreAuthorize("hasAuthority('user:delete')")
-    public void deleteUser(final Authentication principal){
+    public void deleteUser(final Authentication principal) {
         this.userService.deleteAccount(getUserId(principal));
     }
 
@@ -69,11 +68,12 @@ public class UserController {
     @PutMapping("/{userId}/permissions")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @PreAuthorize("hasAuthority('admin:write')")
-    public void updateUserPermissions(@PathVariable String userId, @RequestBody @Valid UserPermissionUpdateRequest request) {
+    public void updateUserPermissions(@PathVariable String userId,
+            @RequestBody @Valid UserPermissionUpdateRequest request) {
         userService.updateUserPermissions(userId, request);
     }
 
-    private String getUserId(final Authentication principal){
+    private String getUserId(final Authentication principal) {
         return ((User) principal.getPrincipal()).getId();
     }
 
