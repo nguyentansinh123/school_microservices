82937b32561153fd730ed423e5d618d1cadf3d96
sinh nguyen <nsinh2281@gmail.com>
2025-11-12 01:42:05 +1100

    analytic service

---
+++ analytic-service/src/test/java/com/caffein/analyticservice/AnalyticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caffein.analyticservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
